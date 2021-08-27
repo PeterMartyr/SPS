02f17affd5b7af21c5e2f5028a5a254b539e9623
Claudio Pietromatire <Claudio.Pietromartire@Student.TafeSa.Edu.AU>
2021-08-27 13:03:19 +0930

    initial  Commit

---
+++ SPS_WEB/SPS_WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SPS_WEB.MvcApplication" Language="C#" %>
---
+++ SPS_WEB/SPS_WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SPS_WEB.Startup))]
+namespace SPS_WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
